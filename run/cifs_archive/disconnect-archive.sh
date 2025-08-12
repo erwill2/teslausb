91d776d1fa25dec9ff3d7bb4711b49ff83c71183
THX723 <25112621+THX723@users.noreply.github.com>
2025-08-11 21:10:23 -0700

    Fix CIFS Mount/Unmount (#990)
    Fixed possible hang when unmounting cifs share with inaccessible network.
    Streamlined the code.

--- run/cifs_archive/disconnect-archive.sh
+++ run/cifs_archive/disconnect-archive.sh
@@ -1,29 +1,29 @@
 #!/bin/bash -eu
 
-# unmount the archive. Without this, the archive mounts can get into a
+# Unmount the archive. Without this, the archive mounts can get into a
 # state where the archive is reachable via the network, appears to be
 # mounted, but the mount is inoperable and any attempt to access it
 # results in a "host is down" message.
 # Run this in the background, since unmounting can hang, which would
 # block a return to archiveloop.
 
-{
-  if [ -e "$ARCHIVE_MOUNT" ]
+unmount_if_set() {
+  local mount_point=$1
+  if [ -n "$mount_point" ]
   then
-    log "unmounting $ARCHIVE_MOUNT"
-    if ! umount -f -l "$ARCHIVE_MOUNT"
+    if findmnt --mountpoint "$mount_point" > /dev/null
     then
-      log "unmount failed"
+      if timeout 10 umount -f -l "$mount_point" >> "$LOG_FILE" 2>&1
+      then
+        log "Unmounted $mount_point."
+      else
+        log "Failed to unmount $mount_point."
+      fi
+    else
+      log "$mount_point already unmounted."
     fi
   fi
+}
 
-  if [ -e "$MUSIC_ARCHIVE_MOUNT" ]
-  then
-    log "unmounting $MUSIC_ARCHIVE_MOUNT"
-    if ! umount -f -l "$MUSIC_ARCHIVE_MOUNT"
-    then
-      log "unmount failed"
-    fi
-  fi
-} &
-
+unmount_if_set "${ARCHIVE_MOUNT:-}" &
+unmount_if_set "${MUSIC_ARCHIVE_MOUNT:-}" &
